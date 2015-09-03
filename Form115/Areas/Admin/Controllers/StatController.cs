4812f0582562e882db7f502a848f432c5b054cf8
scamara <camarasarta@gmail.com>
2015-09-03 22:56:21 +0200

    Ajout de la vue partielle Stattistiques Admin
    Nombre de sejours les plus consultés

---
+++ Form115/Areas/Admin/Controllers/StatController.cs
@@ -0,0 +1,32 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Form115.Areas.Admin.Controllers
+{
+    public class StatController : Controller
+    {
+        [ChildActionOnly]
+        public PartialViewResult Statistiques()
+        {
+            var db = new Form115Entities();
+
+            var listProduitVisites = db.HotelTracking
+                    .GroupBy(h=>h.Hotels.Nom)
+                        .Select(g => new { NomHotel = g.Key, NbVis = g.Count() })
+                .OrderByDescending(x => x.NbVis)
+                 .Take(10)
+                 .AsEnumerable()
+                 .Select(x => new Tuple<string, int>(x.NomHotel, x.NbVis))
+                 .ToList();
+           
+            return PartialView("_AdminStats", listProduitVisites);
+            
+            
+            
+        }
+    }
+}
