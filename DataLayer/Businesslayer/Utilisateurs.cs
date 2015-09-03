6fd1881fd97bd4cdde63c3ee9762aab74090ef39
Sébastien Jacques <sebastien.libre@free.fr>
2015-09-03 17:43:13 +0200

    Classe partielle Utilisateurs pour règles métier réduction + autre

--- DataLayer/Businesslayer/Utilisateurs.cs
+++ DataLayer/Businesslayer/Utilisateurs.cs
@@ -1,12 +1,71 @@
-﻿using System;
+﻿using DataLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataLayer.Businesslayer
+namespace DataLayer.Models
 {
     public partial class Utilisateurs
     {
+
+        Form115Entities db = new Form115Entities();
+
+        public bool EstClient
+        {
+            get
+            {
+                return db.Reservations.Any();
+            }
+        }
+
+        public int getReducBase()
+        {
+            if (Reservations
+                    .Where(resa => resa.Produits.DateDepart >= DateTime.Now.AddMonths(-6))
+                    .Sum(resa => resa.Produits.Prix)
+                    > 150)
+            {
+                return 20;
+            }
+            else if (Reservations
+                  .Where(resa => resa.Produits.DateDepart >= DateTime.Now.AddYears(-1))
+                  .Sum(resa => resa.Produits.Prix)
+                  > 250)
+            {
+                return 15;
+            }
+            else if (Reservations
+                  .Where(resa => resa.Produits.DateDepart >= DateTime.Now.AddYears(-2))
+                  .Sum(resa => resa.Produits.Prix)
+                  > 400)
+            {
+                return 10;
+            }
+            else if (EstClient)
+            {
+                return 5;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int getReducTemp()
+        {
+            if (Reservations
+                .Where(resa => resa.Produits.DateDepart >= DateTime.Now.AddMonths(-4))
+                .Any())
+            {
+                return 5;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
     }
 }
