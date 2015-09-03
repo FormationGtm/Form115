9ee8554f58c5678fd6fde21e9bf7420eda9dd401
Sébastien Jacques <sebastien.libre@free.fr>
2015-09-03 15:04:09 +0200

    Factorisation des Préselectiosn de listes déroulantes

---
+++ Form115/Scripts/Form115.js
@@ -0,0 +1,4 @@
+﻿function selectionnerOption(idListeDeroulante, valueOption) {
+    var selecteur = "#"+idListeDeroulante+" > option[value=" +valueOption + "]";
+    $(selecteur).attr("selected", "selected");
+}
