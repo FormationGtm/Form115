3c1695180176fb75d05c418cc177fb574a4bf408
Sébastien Jacques <sebastien.libre@free.fr>
2015-09-04 16:36:45 +0200

    Revert "Revert "Passage de la réservation d'un séjour""
    This reverts commit 5425ce254be7ba1aac22a5040b7f77524e6c52b4.

---
+++ Form115/Controllers/ReservationsController.cs
@@ -0,0 +1,31 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Form115.Controllers
+{
+    public class ReservationsController : Controller
+    {
+       public readonly Form115Entities db = new Form115Entities();
+
+        // GET: Reservations
+        public ActionResult Reserver(int? id, int? quantite)
+        {
+
+            var verif = db.Produits.Where(p => p.IdSejour == 1003).First();
+
+            if (verif.NbPlaces > quantite && verif.NbPlaces - quantite > 0)
+            {
+                
+                return View("Reserver");
+            }
+            else
+            {
+                return View("ReserverImpossible");
+            }
+        }
+    }
+}
